ce26ce4f885f729e43adefee4f17c3c8fed91507
Justin Love <git@JustinLove.name>
2014-11-01 20:08:36 -0500

    add offense attribute to two units

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+var spec = require('./lib/spec')
+var prompt = require('prompt')
+prompt.start()
+
+var modPath = '../../server_mods/com.wondible.pa.hotfix_select_all_land/'
+var stream = 'stable'
+var media = require('./lib/path').media(stream)
+
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    copy: {
+      mod: {
+        files: [
+          {
+            src: [
+              'modinfo.json',
+              'LICENSE.txt',
+              'README.md',
+              'CHANGELOG.md',
+              'ui/**',
+              'pa/**'],
+            dest: modPath,
+          },
+        ],
+      },
+    },
+    clean: ['pa', modPath],
+    // copy files from PA, transform, and put into mod
+    proc: {
+      units: {
+        targets: [
+          'pa/units/land/aa_missile_vehicle/aa_missile_vehicle.json',
+          'pa/units/land/bot_bomb/bot_bomb.json'
+        ],
+        process: function(spec) {
+          spec.unit_types.push('UNITTYPE_Offense')
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerMultiTask('proc', 'Process unit files into the mod', function() {
+    if (this.data.targets) {
+      var specs = spec.copyPairs(grunt, this.data.targets, media)
+      spec.copyUnitFiles(grunt, specs, this.data.process)
+    } else {
+      var specs = this.filesSrc.map(function(s) {return grunt.file.readJSON(media + s)})
+      var out = this.data.process.apply(this, specs)
+      grunt.file.write(this.data.dest, JSON.stringify(out, null, 2))
+    }
+  })
+
+  // Default task(s).
+  grunt.registerTask('default', ['proc', 'copy:mod']);
+
+};
+
